e457d6cbc2aad96d2c527d6178b2f7e803ed1835
Jean-Louis <jcroes02@gmail.com>
2025-09-29 14:57:46 +0200

    ive added a sawtooth signal

--- gen_waves.py
+++ gen_waves.py
@@ -12,3 +12,9 @@ def generate_square_signal(A,F,duration,sample_rate):
     return A*signal.square(np.pi*F*t)
 
 plt.plot(generate_square_signal(10,1,10,100))
+
+def gen_sawtooth_signal(A,F,duration,sample_rate):
+    t=np.linspace(0,duration,int(sample_rate))
+    return A*signal.sawtooth(2*np.pi*F*t)
+
+plt.plot(gen_sawtooth_signal(10,1,10,100000))
