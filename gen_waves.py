8a2091c4a24e8fcc423e5d75fc3d440fd01c70f1
Jean-Louis <jcroes02@gmail.com>
2025-10-01 16:51:57 +0200

    made return of every function shorter.

--- gen_waves.py
+++ gen_waves.py
@@ -2,18 +2,20 @@ import numpy as np
 from scipy import signal
 
 def gen_sine_wave(A,F,duration, sample_rate):
-    t=np.linspace(0,duration, int(sample_rate))
-    return A*np.sin(2*np.pi*F*t)
+    t=np.linspace(0,duration,sample_rate)
+    return t,A*np.sin(2*np.pi*F*t)
 
 def gen_square_signal(A,F,duration,sample_rate):
-    t=np.linspace(0,duration,int(sample_rate))
-    return A*signal.square(np.pi*F*t)
+    t=np.linspace(0,duration,sample_rate)
+    A*signal.square(np.pi*F*t)
+    return t,A*signal.square(np.pi*F*t)
 
 def gen_sawtooth_signal(A,F,duration,sample_rate):
-    t=np.linspace(0,duration,int(sample_rate))
-    return A*signal.sawtooth(2*np.pi*F*t)
+    t=np.linspace(0,duration,sample_rate)
+    v=A*signal.sawtooth(2*np.pi*F*t)
+    return t,v
 
-def gen_step(A,L,S,E,sample_rate):
-    t=np.linspace(S,E,sample_rate)
+def gen_step(A,L,duration,sample_rate):
+    t=np.linspace(0,duration,sample_rate)
     v=A*np.where(t>L,1,0)
     return t,v
